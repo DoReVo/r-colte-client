ea6e7c80ec227d784f828a9be1011fe48bb90ccd
DoReVo <frz565@gmail.com>
2022-03-07 03:05:25 +0800

    add more stuff

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import { setupLayouts } from "virtual:generated-layouts";
+import VirtualPages from "~pages";
 
-createApp(App).mount('#app')
+const RoutesWithLayouts = setupLayouts(VirtualPages);
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: RoutesWithLayouts,
+});
+
+const app = createApp(App);
+
+app.use(router);
+app.use(createPinia());
+
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, UserConfigExport } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Pages from "vite-plugin-pages";
+import HtmlConfig from "vite-plugin-html-config";
+import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
+import Layouts from "vite-plugin-vue-layouts";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+
+export default ({ mode }) => {
+  // Conditional stuff such as including analytic script only in production
+  // use the mode variable
+  // Example - if mode === production, include analytic script
+
+  const config: UserConfigExport = defineConfig({
+    plugins: [
+      vue(),
+      Pages({ routeStyle: "nuxt" }),
+      // Inject analytics script / meta tags into the head
+      // by modifying this part
+      // Reference - https://github.com/ahwgs/vite-plugin-html-config
+      HtmlConfig({}),
+      // Import google fonts here, https://fonts.google.com/
+      // Reference - https://github.com/feat-agency/vite-plugin-webfont-dl
+      ViteWebfontDownload([]),
+      // Layouts like nuxt
+      // Reference - https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+      Layouts(),
+    ],
+  });
+
+  return config;
+};
