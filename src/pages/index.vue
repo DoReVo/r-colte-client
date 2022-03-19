90ae0318fa7ccc252952496c5627876e6325f024
DoReVo <frz565@gmail.com>
2022-03-20 02:19:47 +0800

    format

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,14 +8,18 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="h-full max-h-full min-h-screen pt-40">
+  <div class="pt-96">
     <div class="mx-auto h-52 w-72 bg-white p-5 shadow">
       <h1 class="text-center text-xl">Prête Template</h1>
       <div class="flex w-full justify-around pt-5">
-        <router-link to="credit" class="text-center text-blue-600 visited:text-blue-800 hover:text-blue-400"
+        <router-link
+          to="credit"
+          class="text-center text-blue-600 visited:text-blue-800 hover:text-blue-400"
           >Credits</router-link
         >
-        <router-link to="signin" class="text-center text-blue-600 visited:text-blue-800 hover:text-blue-400"
+        <router-link
+          to="signin"
+          class="text-center text-blue-600 visited:text-blue-800 hover:text-blue-400"
           >Sign in</router-link
         >
       </div>
