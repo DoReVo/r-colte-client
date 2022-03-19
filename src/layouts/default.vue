a411b534ea7498ebf0267c300a533d1df6f1601a
DoReVo <frz565@gmail.com>
2022-03-20 02:19:58 +0800

    give name to component

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,3 +1,13 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "DefaultTemplate",
+});
+</script>
+
 <template>
   <router-view />
 </template>
+
+<style scoped lang="scss"></style>
